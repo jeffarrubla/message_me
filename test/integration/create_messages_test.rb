b1a45c15c6d25ba25e8ba598a9f531d7557f9eeb
Jefferson <jeffarrubla@gmail.com>
2020-01-22 20:22:19 +0000

    Add testing for add messages

---
+++ test/integration/create_messages_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class CreateMessagesTest < ActionDispatch::IntegrationTest
+	
+	def setup
+		@user = User.create(username: "john", password: "password")
+	end
+
+	test "should create a new message" do 
+		sign_in_as(@user,"password")
+		get root_path
+		assert_template 'chatroom/index'
+		assert_difference 'Message.count', 1 do
+			post message_path, params: {message: {body: "This is a message from test"}}
+			follow_redirect!
+		end
+		assert_template 'chatroom/index'
+		assert_match "This is a message from test", response.body
+	end	
+end
