e3fa784cf848b6b9bb776b64fb48f4c8b3bfdd76
Jefferson <jeffarrubla@gmail.com>
2020-01-22 19:37:59 +0000

    Enable flash messages display

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,4 +21,8 @@ import 'semantic-ui-sass';
 
 $(document).on('turbolinks:load', function(){
 	$('.ui.dropdown').dropdown();
-})
+	$('.message .close').on('click', function() {
+	    $(this).closest('.message').transition('fade');
+	});
+});
+
