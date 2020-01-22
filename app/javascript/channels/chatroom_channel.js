2ed49b50877fdca814048ec72ea0e6f138bb2328
Jefferson <jeffarrubla@gmail.com>
2020-01-22 21:21:35 +0000

    Display messages using partials

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -11,6 +11,6 @@ consumer.subscriptions.create("ChatroomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    alert( data.foo)
+    $('#message-container').append(data.mod_message);
   }
 });
